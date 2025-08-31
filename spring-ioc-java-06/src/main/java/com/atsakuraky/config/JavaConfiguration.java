174af3d4ea4f83a032d0bdaad2bbede9870fdf63
SakuraKy <2790409858@qq.com>
2025-08-31 19:19:54 +0800

    @bean注解使用

--- spring-ioc-java-06/src/main/java/com/atsakuraky/config/JavaConfiguration.java
+++ spring-ioc-java-06/src/main/java/com/atsakuraky/config/JavaConfiguration.java
@@ -1,6 +1,9 @@
 package com.atsakuraky.config;
 
 
+import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -19,4 +22,33 @@ java的配置类，替代xml配置文件
 @PropertySource(value = "classpath:jdbc.properties") //引用外部配置文件
 @Configuration
 public class JavaConfiguration {
+
+    @Value("${atsakuraky.url}")
+    private String url;
+    @Value("${atsakuraky.driver}")
+    private String driver;
+    @Value("${atsakuraky.username}")
+    private String username;
+    @Value("${atsakuraky.password}")
+    private String password;
+
+
+    /*
+    第三方bean标签
+    <bean id="" class=""> 在配置类就是一个方法
+    方法返回值类型就是bean组件的类型或者它的接口和父类
+    方法名称 = <bean id="">
+    方法体可以自定义实现过程
+    最重要的一步：在方法上添加注解@bean 会真正让配置类方法创建的组件存储到ioc容器中
+
+     */
+    @Bean
+    public DruidDataSource dataSource(){
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setDriverClassName(driver);
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        return dataSource;
+    }
 }
