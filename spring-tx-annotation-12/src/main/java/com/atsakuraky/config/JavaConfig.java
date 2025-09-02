bc2286a2fa9ae8156e09c03ee4625c5ea14522da
SakuraKy <2790409858@qq.com>
2025-09-02 13:59:12 +0800

    事务注释添加实践

--- spring-tx-annotation-12/src/main/java/com/atsakuraky/config/JavaConfig.java
+++ spring-tx-annotation-12/src/main/java/com/atsakuraky/config/JavaConfig.java
@@ -2,17 +2,19 @@ package com.atsakuraky.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.*;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.TransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan("com.atsakuraky")
 @PropertySource("classpath:jdbc.properties")
+@EnableAspectJAutoProxy  //开启aspectJ注解的支持
+@EnableTransactionManagement  //开启事务注解的支持
 public class JavaConfig {
 
     @Value("${atsakuraky.driver}")
@@ -41,5 +43,14 @@ public class JavaConfig {
         JdbcTemplate jdbcTemplate = new JdbcTemplate();
         jdbcTemplate.setDataSource(dataSource);
         return jdbcTemplate;
+    }
+
+    @Bean
+    public TransactionManager transactionManager(DataSource dataSource){
+        //内部进行事务操作，基于连接池
+        DataSourceTransactionManager dataSourceTransactionManager = new DataSourceTransactionManager();
+        //需要连接池对象
+        dataSourceTransactionManager.setDataSource(dataSource);
+        return dataSourceTransactionManager;
     }
 }
--- spring-tx-annotation-12/src/main/java/com/atsakuraky/service/StudentService.java
+++ spring-tx-annotation-12/src/main/java/com/atsakuraky/service/StudentService.java
@@ -3,13 +3,21 @@ package com.atsakuraky.service;
 import com.atsakuraky.dao.StudentDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class StudentService {
     
     @Autowired
     private StudentDao studentDao;
-    
+
+    /*
+    添加事务：@Transactional
+    位置：方法 / 类
+    方法：当前方法有事务
+    类：当前类的所有方法都有事务
+     */
+    @Transactional
     public void changeInfo(){
         studentDao.updateAgeById(100,1);
         System.out.println("-----------");
