362bc213077b5713c521869f373563349fee27e9
SakuraKy <2790409858@qq.com>
2025-09-02 14:06:13 +0800

    只读模式设置介绍和设置

--- spring-tx-annotation-12/src/main/java/com/atsakuraky/service/StudentService.java
+++ spring-tx-annotation-12/src/main/java/com/atsakuraky/service/StudentService.java
@@ -16,11 +16,27 @@ public class StudentService {
     位置：方法 / 类
     方法：当前方法有事务
     类：当前类的所有方法都有事务
+
+
+    1.只读模式
+    只读模式可以提升查询事务的效率，推荐事务只有查询代码时使用
+    默认：boolen readOnly() default false;
+    解释：一般情况下，都是通过类添加注解添加事务
+         类下的所有方法都有事务
+         查询方法可以通过再次添加注解，设置为只读模式
+
+
      */
     @Transactional
     public void changeInfo(){
         studentDao.updateAgeById(100,1);
         System.out.println("-----------");
         studentDao.updateNameById("test1",1);
+    }
+
+    @Transactional(readOnly = true)
+    public void getStudent(){
+        //获取学生信息
+
     }
 }
